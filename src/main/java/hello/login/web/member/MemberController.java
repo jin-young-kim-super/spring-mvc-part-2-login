c87b3852519d5c4fdd917712d05d957167e782ab
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-15 06:48:33 +0900

    新規会員登録

---
+++ src/main/java/hello/login/web/member/MemberController.java
@@ -0,0 +1,43 @@
+package hello.login.web.member;
+
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    // @ModelAttribute가 필요한 이유 : 폼 화면에서 정보를 객체에 받기 위함
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "/members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Validated @ModelAttribute Member member, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "/members/addMemberForm";
+        }
+
+        memberRepository.save(member);
+
+        return "redirect:/";
+    }
+
+
+}
