08969b90eaebc63746a24bf12fadf1ede21af8f3
Jin Young Kim <wlsdud66523@kakao.com>
2026-06-20 08:16:45 +0900

    ログイン処理 - サーブレット HTTP セッション2

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -5,13 +5,13 @@ import hello.login.domain.member.MemberRepository;
 import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -63,7 +63,7 @@ public class HomeController {
         return "/loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -76,4 +76,19 @@ public class HomeController {
         model.addAttribute("member",loginMember);
         return "/loginHome";
     }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // @SessionAttribute = request.getSession(false) + (Member)session.getAttribute(SessionConst.LOGIN_MEMBER)
+        // -> 세션을 활용하기 위해 일일이 HttpSevletRequest를 사용하는 게 번거롭다. 그래서 @SessionAttribute(스프링이 제공)을 사용하면 코드가 훨씬 깔끔해 진다.
+        // @SessionAttribute는 세션을 조회만 하지 새롭게 생성을 하지는 않는다. 그래서 이미 로그인한 유저의 세션을 다루고 싶을 떄만 사용 가능!
+        if(loginMember == null) {
+            return "/home";
+        }
+
+        model.addAttribute("member",loginMember);
+        return "/loginHome";
+    }
+
 }
